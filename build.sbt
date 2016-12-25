3c9bf43abad1a5456463684ec03a6567c5be0e05
Zoltan Albrecht <zoltan.albrecht@sap.com>
2016-12-25 22:39:05 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "frp_sc2"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/com/bsdoublestar/frp/Signal.scala
@@ -0,0 +1,8 @@
+package com.bsdoublestar.frp
+
+/**
+  * Created by bsdoublestar on 12/25/16.
+  */
+class Signal {
+
+}
